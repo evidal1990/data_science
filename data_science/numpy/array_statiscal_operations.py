948cd796bc5b1a7ad46fa62136d577f320dafd28
evidal1990 <ecsr.vidal@gmail.com>
2024-12-05 09:09:05 -0300

    Exercícios de operações estatísticas 6/10 concluídos

---
+++ data_science/numpy/array_statiscal_operations.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+# 01. Crie um array e calcule a mediana dos elementos.
+array_stats_ex01 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.median(array_stats_ex01)}')
+print("---------------------------------------------------------")
+
+# 02. Crie um array e calcule a variância dos elementos.
+array_stats_ex02 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.var(array_stats_ex02)}')
+print("---------------------------------------------------------")
+
+# 03. Crie um array e calcule o desvio padrão.
+array_stats_ex03 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.round(np.std(array_stats_ex03))}')
+print("---------------------------------------------------------")
+
+# 04. Crie um array e calcule o valor máximo.
+array_stats_ex04 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.max(array_stats_ex04)}')
+print("---------------------------------------------------------")
+
+# 05. Crie um array e calcule o valor mínimo.
+array_stats_ex05 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.min(array_stats_ex05)}')
+print("---------------------------------------------------------")
+
+# 06. Crie um array e calcule a soma acumulada.
+array_stats_ex06 = np.array([50, 20, 30, 10, 40])
+print(f'Resultado esperado = {np.cumsum(array_stats_ex06)}')
+print("---------------------------------------------------------")
+
+# 07. Crie um array e calcule a soma dos elementos de cada coluna.
+print("---------------------------------------------------------")
+
+# 08. Crie um array e calcule a soma dos elementos de cada linha.
+print("---------------------------------------------------------")
+
+# 09. Crie um array e calcule a distribuição dos elementos (percentis).
+print("---------------------------------------------------------")
+
+# 10. Crie um array e calcule a média dos elementos.
+print("---------------------------------------------------------")
